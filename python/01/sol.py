4746fba0c31b33eaddef77fa530a45b901e7bd14
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-01 13:51:20 +0200

    Day 1 - add alternative solution

--- python/01/sol.py
+++ python/01/sol.py
@@ -6,6 +6,7 @@ from puzzle import PuzzleContext
 from utils import *
 import itertools as itt
 import functools as ft
+import re
 
 def find_first(s: str, digit_mapping: dict[str, int]) -> int:
     positions = [
@@ -26,6 +27,17 @@ def find_last(s: str, digit_mapping: dict[str, int]) -> int:
 def get_calibration_value(line: str, digit_mapping: dict[str, int]) -> int:
     a = find_first(line, digit_mapping)
     b = find_last(line, digit_mapping)
+    return 10*a + b
+
+# alternative solution using regex
+def get_calibration_value_2(line: str, digit_mapping: dict[str, int]) -> int:
+    pattern = "|".join(digit_mapping.keys())
+    pattern = f"(?=({pattern}))"
+    # generates this for the second part
+    # (?=(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))
+    matches = re.findall(pattern, line)
+    a = digit_mapping[matches[0]]
+    b = digit_mapping[matches[-1]]
     return 10*a + b
 
 with PuzzleContext(year=2023, day=1) as ctx:
