99470533bb5fccfc44ef712fb352ac0c11f2c840
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-01 13:37:48 +0200

    Day 1 - cleanup

--- python/01/sol.py
+++ python/01/sol.py
@@ -7,57 +7,41 @@ from utils import *
 import itertools as itt
 import functools as ft
 
+def find_first(s: str, digit_mapping: dict[str, int]) -> int:
+    positions = [
+        (pos, v)
+        for k, v in digit_mapping.items()
+        if (pos := s.find(k)) != -1
+    ]
+    return min(positions, key=lambda x: x[0])[1]
+
+
+def find_last(s: str, digit_mapping: dict[str, int]) -> int:
+    return find_first(
+        s[::-1],
+        {k[::-1]: v for k, v in digit_mapping.items()}
+    )
+
+
+def get_calibration_value(line: str, digit_mapping: dict[str, int]) -> int:
+    a = find_first(line, digit_mapping)
+    b = find_last(line, digit_mapping)
+    return 10*a + b
 
 with PuzzleContext(year=2023, day=1) as ctx:
-    ans1, ans2 = None, None
+    ans1, ans2 = 0, 0
 
-    # ans1 = 0 
-    # for line in ctx.nonempty_lines:
-    #     digits = [c for c in line if c in "0123456789"]
-    #     cv = digits[0] + digits[-1]
-    #     ans1 += int(cv)
+    DIGITS = {str(i): i for i in range(1, 10)}
+    NAMES = {
+        name: i+1
+        for i, name in enumerate(
+            ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        )
+    }
 
-    ans2 = 0 
     for line in ctx.nonempty_lines:
-        r = {
-            "one": "1",
-            "two": "2",
-            "three": "3",
-            "four": "4",
-            "five": "5",
-            "six": "6",
-            "seven": "7",
-            "eight": "8",
-            "nine": "9",
-            "1": "1",
-            "2": "2",
-            "3": "3",
-            "4": "4",
-            "5": "5",
-            "6": "6",
-            "7": "7",
-            "8": "8",
-            "9": "9",
-        }
-        first = None
-        fa = None
-        last = None
-        la = None
-        for x in r.items():
-            i = line.find(x[0])
-            j = line[::-1].find(x[0][::-1])
-            if i == -1 or j == -1:
-                continue
-            if first is None or i < first:
-                first = i
-                fa = x[1]
-            if last is None or j < last:
-                last = j
-                la = x[1]
-        cv = fa + la
-        # cv = digits[0] + digits[-1]
-        print(line, cv)
-        ans2 += int(cv)
+        ans1 += get_calibration_value(line, DIGITS)
+        ans2 += get_calibration_value(line, DIGITS | NAMES)
 
     ctx.submit(1, str(ans1) if ans1 else None)
     ctx.submit(2, str(ans2) if ans2 else None)
