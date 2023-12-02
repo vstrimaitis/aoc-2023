667fa1699f5c8aac9735addc250164cbe0f5837a
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-02 07:19:20 +0200

    Day 2 - refactor

--- python/02/sol.py
+++ python/02/sol.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections import *
 from typing import *
 from heapq import *
@@ -7,44 +8,64 @@ from utils import *
 import itertools as itt
 import functools as ft
 
+@dataclass
+class Draw:
+    blue: int
+    red: int
+    green: int
+
+    @classmethod
+    def from_str(cls, s: str) -> Draw:
+        counts = {
+            parts[1]: int(parts[0])
+            for cnt_descr in s.split(", ")
+            if (parts := cnt_descr.split(" "))
+        }
+        return Draw(
+            blue=counts.get("blue", 0),
+            red=counts.get("red", 0),
+            green=counts.get("green", 0)
+        )
+    
+    @property
+    def power(self) -> int:
+        return self.red * self.green * self.blue
+
+@dataclass
+class Game:
+    id_: int
+    draws: list[Draw]
+
+    @classmethod
+    def from_str(cls, s: str) -> Game:
+        id_descr, draws_descr = s.split(": ")
+        return Game(
+            id_=int(id_descr.split(" ")[1]),
+            draws=[Draw.from_str(s) for s in draws_descr.split("; ")]
+        )
+    
+    @property
+    def minimizing_draw(self) -> Draw:
+        return ft.reduce(
+            lambda a, b: Draw(
+                red=max(a.red, b.red),
+                green=max(a.green, b.green),
+                blue=max(a.blue, b.blue),
+            ),
+            self.draws,
+            Draw(0, 0, 0)
+        )
+
 
 with PuzzleContext(year=2023, day=2) as ctx:
-    ans1, ans2 = None, None
+    ans1, ans2 = 0, 0
 
-    ans1 = 0
-    for line in ctx.nonempty_lines:
-        id, descr = line.split(": ")
-        id = int(id.split(" ")[1])
-        subsets = descr.split("; ")
-        is_possible = True
-        for s in subsets:
-            counts = {}
-            for ss in s.split(", "):
-                x, col = ss.split(" ")
-                x = int(x)
-                counts[col] = x
-            if counts.get("red", 0) > 12 or counts.get("green", 0) > 13 or counts.get("blue", 0) > 14:
-                is_possible = False
-        if is_possible:
-            ans1 += id
+    games = list(map(Game.from_str, ctx.nonempty_lines))
 
-    ans2 = 0
-    for line in ctx.nonempty_lines:
-        id, descr = line.split(": ")
-        id = int(id.split(" ")[1])
-        subsets = descr.split("; ")
-        mins = {"red": 0, "green": 0, "blue": 0}
-        for s in subsets:
-            counts = {}
-            for ss in s.split(", "):
-                x, col = ss.split(" ")
-                x = int(x)
-                counts[col] = x
-            mins["red"] = max(mins["red"], counts.get("red", 0))
-            mins["green"] = max(mins["green"], counts.get("green", 0))
-            mins["blue"] = max(mins["blue"], counts.get("blue", 0))
-        x = mins["red"] * mins["green"] * mins["blue"]
-        ans2 += x
+    for game in games:
+        if all(d.red <= 12 and d.green <= 13 and d.blue <= 14 for d in game.draws):
+            ans1 += game.id_
+        ans2 += game.minimizing_draw.power
 
     ctx.submit(1, str(ans1) if ans1 else None)
     ctx.submit(2, str(ans2) if ans2 else None)
