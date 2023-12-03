8a202a2919a36fd3df20204d551c40d12292f0cd
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-03 07:41:58 +0200

    Day 3 - refactor

--- python/03/sol.py
+++ python/03/sol.py
@@ -1,47 +1,77 @@
+from __future__ import annotations
 from collections import *
 from typing import *
 from heapq import *
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from puzzle import PuzzleContext
 from utils import *
 import itertools as itt
 import functools as ft
 import re
 
+Coords = Tuple[int, int]
+
+@dataclass
+class Symbol:
+    value: str
+    coords: Coords
+
+@dataclass
+class Number:
+    value: int
+    coords: Coords
+    adjacent_symbols: list[Symbol] = field(default_factory=lambda: [])
+
+    @classmethod
+    def from_position(cls, grid: list[str], row: int, col: int, value: int) -> Number:
+        coords = (row, col)
+        col_end = col+len(str(value))-1
+        number_coords = set([(row, j) for j in range(col, col_end+1)])
+        adj_coords = [
+            (i, j)
+            for i in range(row-1, row+2)
+            for j in range(col-1, col_end+2)
+            if (
+                0 <= i < len(grid) and
+                0 <= j < len(grid[i]) and
+                (i, j) not in number_coords
+            )
+        ]
+        adj_symbols = []
+        for (i, j) in adj_coords:
+            c = grid[i][j]
+            if c not in ".0123456789":
+                adj_symbols.append(Symbol(c, (i, j)))
+        return Number(value, coords, adj_symbols)
+
+def extract_numbers(grid: list[str]) -> list[Number]:
+    nums = []
+    for i, line in enumerate(grid):
+        for m in re.finditer(r"\d+", line):
+            nums.append(Number.from_position(grid, i, m.span()[0], int(m.group(0))))
+    return nums
+
 with PuzzleContext(year=2023, day=3) as ctx:
     ans1, ans2 = None, None
 
-    grid = ctx.nonempty_lines
-    ans1 = 0
-    mp = defaultdict(lambda: defaultdict(lambda: []))
-    for i, line in enumerate(grid):
-        for m in re.finditer(r"\d+", line):
-            if m:
-                j1 = m.span()[0]
-                j2 = m.span()[1]
-                x = int(m.group(0))
-                ok = False
-                for j in range(j1, j2):
-                    for di in [-1, 0, 1]:
-                        for dj in [-1, 0, 1]:
-                            if di == 0 and dj == 0:
-                                continue
-                            ii = i + di
-                            jj = j + dj
-                            if 0 <= ii < len(grid) and 0 <= jj < len(line):
-                                if grid[ii][jj] not in ".0123456789":
-                                    if not ok:
-                                        mp[grid[ii][jj]][(ii, jj)].append(x)
-                                    ok = True
-                if ok:
-                    # print(x)
-                    # input()
-                    ans1 += x
+    numbers = extract_numbers(ctx.nonempty_lines)
+    
+    ans1 = sum(
+        num.value
+        for num in numbers
+        if len(num.adjacent_symbols) > 0
+    )
 
-    ans2 = 0
-    for p in mp["*"].values():
-        if len(p) == 2:
-            ans2 += p[0] * p[1]
+    groups = defaultdict(lambda: [])
+    for num in numbers:
+        for symbol in num.adjacent_symbols:
+            if symbol.value == "*":
+                groups[symbol.coords].append(num.value)
+    ans2 = sum(
+        g[0] * g[1]
+        for g in groups.values()
+        if len(g) == 2
+    )
 
     ctx.submit(1, str(ans1) if ans1 else None)
     ctx.submit(2, str(ans2) if ans2 else None)
