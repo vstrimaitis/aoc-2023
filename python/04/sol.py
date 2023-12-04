0dcd837dd8a51924cb24542d4c8df9258ce63b72
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-04 07:18:28 +0200

    Day 4 - refactor

--- python/04/sol.py
+++ python/04/sol.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections import *
 from typing import *
 from heapq import *
@@ -8,37 +9,49 @@ import itertools as itt
 import functools as ft
 import re
 
+@dataclass
+class Card:
+    winning_nums: set[int]
+    nums: list[int]
+
+    @classmethod
+    def from_str(cls, s: str) -> Card:
+        parts = s.split(" | ")
+        parts[0] = parts[0].split(": ", 1)[1]
+        return Card(
+            winning_nums=set(extract_numbers(parts[0])),
+            nums=extract_numbers(parts[1])
+        )
+    
+    @property
+    def match_count(self) -> int:
+        return sum(1 for x in self.nums if x in self.winning_nums)
+    
+    @property
+    def points(self) -> int:
+        if (m := self.match_count) > 0:
+            return 2 ** (m - 1)
+        return 0
+    
+def extract_numbers(s: str) -> list[int]:
+    return [
+        int(x)
+        for x in re.findall(r"\d+", s)
+    ]
 
 with PuzzleContext(year=2023, day=4) as ctx:
-    ans1, ans2 = None, None
-
-    ans1 = 0
-    for l in ctx.nonempty_lines:
-        parts = l.split(" | ")
-        winning = set([int(x) for x in re.findall(r"\d+", parts[0].split(": ")[1])])
-        have = [int(x) for x in re.findall(r"\d+", parts[1])]
-        pts = 0
-        for x in have:
-            if x in winning:
-                if pts == 0:
-                    pts = 1
-                else:
-                    pts *= 2
-        ans1 += pts
+    cards = list(map(Card.from_str, ctx.nonempty_lines))
+    ans1 = sum([c.points for c in cards])
 
     
+    counts = [1 for _ in range(len(cards))]
     ans2 = 0
-    counts = defaultdict(lambda: 1)
-    for i, l in enumerate(ctx.nonempty_lines):
-        _ = counts[i]
-        parts = l.split(" | ")
-        winning = set([int(x) for x in re.findall(r"\d+", parts[0].split(": ")[1])])
-        have = [int(x) for x in re.findall(r"\d+", parts[1])]
-        matches = sum(1 for x in have if x in winning)
-        for j in range(matches):
-            counts[i+j+1] += counts[i]
-    ans2 = sum(counts.values())
-        
+    for i, c in enumerate(cards):
+        for d in range(c.match_count):
+            j = i+d+1
+            if j < len(counts):
+                counts[j] += counts[i]
+    ans2 = sum(counts)
 
     ctx.submit(1, str(ans1) if ans1 else None)
     ctx.submit(2, str(ans2) if ans2 else None)
