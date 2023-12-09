3b52fc56a41b55262a24934dcd4d644617fdfc61
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-09 07:29:28 +0200

    Day 9 - refactor

--- python/09/sol.py
+++ python/09/sol.py
@@ -7,46 +7,31 @@ from utils import *
 import itertools as itt
 import functools as ft
 
-def predict(arr: list[int]) -> int:
+def calc_diff_lists(arr: list[int]) -> list[list[int]]:
     lists = [arr]
-    while True:
-        new_list = []
-        prev_list = lists[-1]
-        for i in range(len(prev_list)-1):
-            new_list.append(prev_list[i+1]-prev_list[i])
-        lists.append(new_list)
-        if all(x == 0 for x in new_list):
-            break
-    return sum(l[-1] for l in lists)
-
-def predict2(arr: list[int]) -> int:
-    lists = [arr]
-    while True:
-        new_list = []
-        prev_list = lists[-1]
-        for i in range(len(prev_list)-1):
-            new_list.append(prev_list[i+1]-prev_list[i])
-        lists.append(new_list)
-        if all(x == 0 for x in new_list):
-            break
-
-    x = 0
-    for i in reversed(range(len(lists))):
-        x = lists[i][0] - x
-    return x
+    while not all(x == 0 for x in lists[-1]):
+        lists.append(
+            [
+                y-x
+                for x, y in zip(lists[-1], lists[-1][1:])
+            ]
+        )
+    return lists
 
 with PuzzleContext(year=2023, day=9) as ctx:
-    ans1, ans2 = None, None
 
-    ans1 = 0
-    for l in ctx.nonempty_lines:
-        pred = predict(ints(l))
-        ans1 += pred
+    histories = lmap(ints, ctx.nonempty_lines)
+    ans1 = sum(
+        sum(dlist[-1] for dlist in calc_diff_lists(hist))
+        for hist in histories
+    )
+    ctx.submit(1, str(ans1))
 
-    ctx.submit(1, str(ans1) if ans1 else None)
-
-    ans2 = 0
-    for l in ctx.nonempty_lines:
-        pred = predict2(ints(l))
-        ans2 += pred
-    ctx.submit(2, str(ans2) if ans2 else None)
+    ans2 = sum(
+        ft.reduce(
+            lambda acc, x: x - acc,
+            reversed([dlist[0] for dlist in calc_diff_lists(hist)])
+        )
+        for hist in histories
+    )
+    ctx.submit(2, str(ans2))
