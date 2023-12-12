0101caae60ac2b61cf10e891dc0d27126953c07c
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-12 11:11:39 +0200

    Day 12 - refactor

--- python/12/sol.py
+++ python/12/sol.py
@@ -7,82 +7,53 @@ from utils import *
 import itertools as itt
 import functools as ft
 
-def gen(s: str, i: int = 0, curr: list[str] = []):
-    if i >= len(s):
-        yield "".join(curr)
-        return
-    if s[i] == "?":
-        for c in ".#":
-            curr.append(c)
-            yield from gen(s, i+1, curr)
-            curr.pop()
-    else:
-        curr.append(s[i])
-        yield from gen(s, i+1, curr)
-        curr.pop()
-
-def solve(s: str, cnts: list[int]) -> int:
+def solve2(s: str, cnts: list[int]) -> int:
+    # add one '.' to make conditions simpler
+    s += "."
     @ft.lru_cache(maxsize=None)
     def dp(i: int, j: int, p: int) -> int:
+        if j >= len(cnts):
+            # all groups of #s have been found - every remaining
+            # char must be either '.' or '?' (will replace with '.')
+            return 1 if all(c != "#" for c in s[i:]) else 0
         if i >= len(s):
-            if j == len(cnts) and p == 0:
-                return 1
-            if j == len(cnts)-1 and p == cnts[-1]:
-                return 1
+            # we reached the end but not all cnts have been exchausted
             return 0
-        if s[i] == "#":
-            return dp(i+1, j, p+1)
-        if s[i] == ".":
+        
+        def dot_case():
             if p == 0:
                 return dp(i+1, j, 0)
-            elif j < len(cnts) and p == cnts[j]:
+            if p == cnts[j]:
                 return dp(i+1, j+1, 0)
             return 0
-        options = []
-        # put #
-        options.append(dp(i+1, j, p+1))
-        # put .
-        if j < len(cnts) and p == cnts[j]:
-            options.append(dp(i+1, j+1, 0))
-        elif p == 0:
-            options.append(dp(i+1, j, 0))
-        return sum(options)
-    return dp(0, 0, 0)
         
+        def hash_case():
+            return dp(i+1, j, p+1)
 
-def matches(s: str, cnts: list[int]):
+        match s[i]:
+            case ".": return dot_case()
+            case "#": return hash_case()
+            case "?": return dot_case() + hash_case()
+            case _: raise ValueError(f"Invalid char: {s[i]}")
+    return dp(0, 0, 0)
 
-    return [len(x) for x in s.split(".") if x] == cnts
+def parse(lines: list[str]) -> list[Tuple[str, list[int]]]:
+    def parse_line(line: str):
+        parts = line.split(" ")
+        return (parts[0], ints(parts[1]))
+    return lmap(parse_line, lines)
 
 with PuzzleContext(year=2023, day=12) as ctx:
-    ans1, ans2 = None, None
+    inputs = parse(ctx.nonempty_lines)
+    
+    ans1 = sum(
+        solve2(pattern, cnts)
+        for pattern, cnts in inputs
+    )
+    ctx.submit(1, str(ans1))
 
-    # ans1 = 0
-    # for i, l in enumerate(ctx.nonempty_lines):
-    #     print(i, l)
-    #     pattern, cnts = l.split(" ")
-    #     cnts = ints(cnts)
-    #     ans = 0
-    #     for s in gen(pattern):
-    #         if matches(s, cnts):
-    #             ans += 1
-    #     ans1 += ans
-
-    # ctx.submit(1, str(ans1) if ans1 else None)
-
-    ans2 = 0
-    for i, l in enumerate(ctx.nonempty_lines):
-        print(i, l)
-        pattern, cnts = l.split(" ")
-        cnts = ints(cnts)
-
-        pattern = "?".join([pattern]*5)
-        cnts = cnts*5
-
-        # ans = 0
-        # for s in gen2(pattern, cnts=cnts):
-        #     ans += 1
-        ans = solve(pattern, cnts)
-        print(ans)
-        ans2 += ans
-    ctx.submit(2, str(ans2) if ans2 else None)
+    ans2 = sum(
+        solve2("?".join([pattern]*5), cnts*5)
+        for pattern, cnts in inputs
+    )
+    ctx.submit(2, str(ans2))
