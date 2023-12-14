282b5ed1f660a633108f21ef67f0fe7e5a034fa0
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-14 08:13:50 +0200

    Day 14 - refactor #2

--- python/14/sol.py
+++ python/14/sol.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections import *
 from typing import *
 from heapq import *
@@ -7,95 +8,106 @@ from utils import *
 import itertools as itt
 import functools as ft
 
-Grid = list[list[str]]
-Height = int
-Width = int
-GridDef = tuple[Grid, Height, Width]
 Direction = Literal["N", "W", "S", "E"]
+DIRS = get_args(Direction)
 
-def rotate_cw(g: Grid, n: Height, m: Width) -> GridDef:
-    n, m = m, n
-    g = [list(reversed(col)) for col in zip(*g)]
-    return g, n, m
+class Grid:
+    def __init__(self, cells: list[list[str]]):
+        self._cells = cells
+        self._n = len(cells)
+        self._m = len(cells[0])
 
-def rotate_ccw(g: Grid, n: Height, m: Width) -> GridDef:
-    for _ in range(3):
-        g, n, m = rotate_cw(g, n, m)
-    return g, n, m
-
-def slide_n(g: Grid, n: Height, m: Width) -> Grid:
-    for j in range(m):
-        avail_spot = 0
-        for i in range(n):
-            if g[i][j] == "#":
-                avail_spot = i + 1
-            elif g[i][j] == "O":
-                g[avail_spot][j], g[i][j] = g[i][j], g[avail_spot][j]
-                avail_spot += 1
-    return g
-
-def slide(g: Grid, n: Height, m: Width, d: Direction) -> Grid:
-    def delegate(to: Direction) -> Grid:
-        g2, n2, m2 = rotate_cw(g, n, m)
-        g2 = slide(g2, n2, m2, to)
-        g2, _, _ = rotate_ccw(g2, n2, m2)
-        return g2
-    match d:
-        case "N": return slide_n([r.copy() for r in g], n, m)
-        case "W": return delegate("N")
-        case "S": return delegate("W")
-        case "E": return delegate("S")
-        case _: assert False
-
-def calc_total_load(g: Grid, n: Height, m: Width) -> int:
-    ans = 0
-    for i in range(n):
+    @classmethod
+    def from_str(cls, s: str) -> Grid:
+        return Grid([list(r) for r in s.split("\n")])
+    
+    def rotate_cw(self) -> Grid:
+        return Grid([
+            list(reversed(col))
+            for col in zip(*self._cells)
+        ])
+    
+    def rotate_ccw(self) -> Grid:
+        g = self
+        for _ in range(3):
+            g = g.rotate_cw()
+        return g
+    
+    def slide(self, direction: Direction) -> Grid:
+        if direction == "N":
+            return self._slide_north()
+        i = DIRS.index(direction)
+        g = self.rotate_cw()
+        g = g.slide(DIRS[i-1])
+        return g.rotate_ccw()
+    
+    def spin(self) -> Grid:
+        g = self
+        for d in DIRS:
+            g = g.slide(d)
+        return g
+    
+    def _slide_north(self) -> Grid:
+        # make a copy to not modify the current instance
+        g = [list(r) for r in self._cells]
+        n = self._n
+        m = self._m
         for j in range(m):
-            if g[i][j] == "O":
-                ans += n-i
-    return ans
-
-def solve1(g: Grid, n: Height, m: Width) -> int:
-    g = slide(g, n, m, "N")
-    return calc_total_load(g, n, m)
-
-def solve2(g: Grid, n: Height, m: Width, total_cycles: int) -> int:
-    def do_cycle(gr: Grid) -> Grid:
-        for d in ("N", "W", "S", "E"):
-            gr = slide(gr, n, m, d)
-        return gr
+            avail_spot = 0
+            for i in range(n):
+                if g[i][j] == "#":
+                    avail_spot = i + 1
+                elif g[i][j] == "O":
+                    g[avail_spot][j], g[i][j] = g[i][j], g[avail_spot][j]
+                    avail_spot += 1
+        return Grid(g)
     
-    def to_str(g: Grid) -> str:
-        return "\n".join("".join(r) for r in g)
+    @property
+    def total_load(self) -> int:
+        ans = 0
+        for i in range(self._n):
+            for j in range(self._m):
+                if self._cells[i][j] == "O":
+                    ans += self._n - i
+        return ans
+
+    def __str__(self) -> str:
+        return "\n".join("".join(r) for r in self._cells)
     
-    seen = dict()
-    loads = []
+    # Enables using Grids as keys in a dict
+    def __hash__(self) -> int:
+        return hash(str(self))
+    
+    def __eq__(self, other) -> bool:
+        return str(self) == str(other)
+
+def solve1(g: Grid) -> int:
+    return g.slide("N").total_load
+
+def solve2(g: Grid, total_cycles: int) -> int:
+    seen: dict[Grid, int] = OrderedDict()
     n_cycles = 0
-
-    seen[to_str(g)] = 0
-    loads.append(calc_total_load(g, n, m))
+    seen[g] = 0
 
     while True:
-        g = do_cycle(g)
-        s = to_str(g)
-        if s in seen:
+        g = g.spin()
+        if g in seen:
             break
         n_cycles += 1
-        seen[s] = n_cycles
-        loads.append(calc_total_load(g, n, m))
-    loop_start = seen[to_str(g)]
+        seen[g] = n_cycles
+    loop_start = seen[g]
     loop_end = n_cycles
     loop_len = loop_end - loop_start + 1
     
     n_cycles_left = (total_cycles - loop_start) % loop_len
     target_dist = loop_start + n_cycles_left
-    return loads[target_dist]
+    return list(seen.keys())[target_dist].total_load
 
 with PuzzleContext(year=2023, day=14) as ctx:
-    g, n, m = to_grid(ctx.data)
+    g = Grid.from_str(ctx.data)
 
-    ans1 = solve1(g, n, m)
+    ans1 = solve1(g)
     ctx.submit(1, str(ans1))
     
-    ans2 = solve2(g, n, m, 1_000_000_000)
+    ans2 = solve2(g, 1_000_000_000)
     ctx.submit(2, str(ans2))
