5407be2ca96d6a87f3552d21644a9c8ca2dda9d3
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-15 07:27:34 +0200

    Day 15 - refactor

--- python/15/sol.py
+++ python/15/sol.py
@@ -7,8 +7,7 @@ from utils import *
 import itertools as itt
 import functools as ft
 
-
-def calc(s: str) -> int:
+def calc_hash(s: str) -> int:
     curr = 0
     for c in s:
         curr += ord(c)
@@ -16,35 +15,55 @@ def calc(s: str) -> int:
         curr = curr % 256
     return curr
 
-with PuzzleContext(year=2023, day=15) as ctx:
-    ans1, ans2 = None, None
+@dataclass
+class Lens:
+    label: str
+    focal_length: int
 
-    ans1 = 0
-    for p in ctx.lines[0].split(","):
-        ans1 += calc(p)
+@dataclass
+class InstalledLens(Lens):
+    box_id: int
+    slot_id: int
+
+    @property
+    def focusing_power(self) -> int:
+        return (self.box_id+1)*(self.slot_id+1)*self.focal_length
+
+class HashMap:
+    def __init__(self, size: int):
+        self._boxes: list[list[Lens]] = [[] for _ in range(size)]
+    
+    def add(self, label: str, value: int) -> None:
+        box_id = calc_hash(label)
+        i = [i for i, l in enumerate(self._boxes[box_id]) if l.label == label]
+        if len(i) == 0:
+            self._boxes[box_id].append(Lens(label, value))
+        else:
+            assert len(i) == 1
+            i = i[0]
+            self._boxes[box_id][i].focal_length = value 
+
+    def remove(self, label: str) -> None:
+        box_id = calc_hash(label)
+        self._boxes[box_id] = [l for l in self._boxes[box_id] if l.label != label]
+
+    @property
+    def installed_lenses(self) -> Iterable[InstalledLens]:
+        for box_id, box in enumerate(self._boxes):
+            for slot_id, lens in enumerate(box):
+                yield InstalledLens(lens.label, lens.focal_length, box_id, slot_id)
+
+with PuzzleContext(year=2023, day=15) as ctx:
+    ans1 = sum(map(calc_hash, ctx.lines[0].split(",")))
     ctx.submit(1, str(ans1) if ans1 else None)
 
-    N = 256
-    lenses: list[list[tuple[str, int]]] = [[] for _ in range(N)]
+    boxes = HashMap(256)
     for p in ctx.lines[0].split(","):
         if "-" in p:
             label = p.split("-")[0]
-            box_id = calc(label)
-            print(label, box_id)
-            lenses[box_id] = [l for l in lenses[box_id] if l[0] != label]
+            boxes.remove(label)
         elif "=" in p:
             label, val = p.split("=", 1)
-            box_id = calc(label)
-            print(label, box_id)
-            val = int(val)
-            i = [i for i, l in enumerate(lenses[box_id]) if l[0] == label]
-            if len(i) > 0:
-                assert len(i) == 1
-                lenses[box_id][i[0]] = (label, val)
-            else:
-                lenses[box_id].append((label, val))
-    ans2 = 0
-    for i in range(N):
-        for i, (box_id, v) in enumerate(lenses[i]):
-            ans2 += (calc(box_id)+1)*(i+1)*v
+            boxes.add(label, int(val))
+    ans2 = sum(l.focusing_power for l in boxes.installed_lenses)
     ctx.submit(2, str(ans2) if ans2 else None)
