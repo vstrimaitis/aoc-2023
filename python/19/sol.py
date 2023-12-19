93ec98dc9c760c93a9b11376fb2c3b4a6d0d5bfa
Vytautas Strimaitis <vstrimaitis@gmail.com>
2023-12-19 13:53:16 +0200

    Day 19 - refactor

--- python/19/sol.py
+++ python/19/sol.py
@@ -1,96 +1,148 @@
+from __future__ import annotations
 from collections import *
 from typing import *
 from heapq import *
 from dataclasses import dataclass
+import dataclasses
 from puzzle import PuzzleContext
 from utils import *
 import itertools as itt
 import functools as ft
+from abc import ABC, abstractmethod
 
-def get_next(conds, ratings) -> str:
-    for cond, nxt in conds:
-        if cond is None:
-            return nxt
-        res = eval(cond, ratings)
-        if res:
-            return nxt
-    assert False
+@dataclass
+class Range:
+    l: int
+    r: int
 
-def is_accepted(ratings, workflows) -> bool:
-    curr = "in"
-    while True:
-        curr = get_next(workflows[curr], ratings)
-        if curr == "R":
-            return False
-        if curr == "A":
-            return True
-        
-def simulate(ratings, workflows, curr) -> int:
-    if any(x > y for x, y in ratings.values()):
+    @property
+    def length(self) -> int:
+        return max(0, self.r - self.l + 1)
+
+@dataclass
+class PartRatings:
+    x: Range
+    m: Range
+    a: Range
+    s: Range
+
+    @property
+    def count(self) -> int:
+        return self.x.length * self.m.length * self.a.length * self.s.length
+    
+    @property
+    def total(self) -> int:
+        return self.x.l + self.m.l + self.a.l + self.s.l
+
+    
+    @classmethod
+    def parse(cls, s: str) -> PartRatings:
+        d = dict()
+        for p in s[1:-1].split(","):
+            field, val = p.split("=", 1)
+            val = int(val)
+            d[field] = Range(val, val)
+        return PartRatings(**d)
+    
+class Node(ABC):
+    @abstractmethod
+    def evaluate(self, ratings: PartRatings) -> int:
+        ...
+
+class AcceptedNode(Node):
+    def evaluate(self, ratings: PartRatings) -> int:
+        return ratings.count
+    
+class RejectedNode(Node):
+    def evaluate(self, ratings: PartRatings) -> int:
         return 0
-    if curr == "A":
-        ans = 1
-        for x, y in ratings.values():
-            ans *= max(0, y-x+1)
-        return ans
-    if curr == "R":
-        return 0
-    conds = workflows[curr]
-    ans = 0
-    for cond, nxt in conds:
-        if cond is None:
-            ans += simulate(ratings, workflows, nxt)
-            break
+    
+class PassthroughNode(Node):
+    def __init__(self, workflow_name: str, index: int, next: Node):
+        self.id_ = f"{workflow_name}-{index}"
+        self._next = next
+
+    def evaluate(self, ratings: PartRatings) -> int:
+        return self._next.evaluate(ratings)
+    
+class RuleNode(Node):
+    def __init__(self, workflow_name: str, index: int, field_name: str, value: int, left: Node, right: Node):
+        self.id_ = f"{workflow_name}-{index}"
+        self._field = field_name
+        self._value = value
+        self._left = left
+        self._right = right
+
+    @abstractmethod
+    def split(self, r: Range) -> tuple[Range, Range]:
+        ...
+
+    def evaluate(self, ratings: PartRatings) -> int:
+        value_range: Range = getattr(ratings, self._field)
+        range_left, range_right = self.split(value_range)
+        ratings_left = dataclasses.replace(ratings, **{self._field: range_left})
+        ratings_right = dataclasses.replace(ratings, **{self._field: range_right})
+        return self._left.evaluate(ratings_left) + self._right.evaluate(ratings_right)
+
+class LessNode(RuleNode):
+    def split(self, r: Range) -> tuple[Range, Range]:
+        left = Range(
+            r.l,
+            min(r.r, self._value-1),
+        )
+        right = Range(
+            max(r.l, self._value),
+            r.r,
+        )
+        return left, right
+
+class GreaterNode(RuleNode):
+    def split(self, r: Range) -> tuple[Range, Range]:
+        left = Range(
+            max(r.l, self._value + 1),
+            r.r,
+        )
+        right = Range(
+            r.l,
+            min(r.r, self._value),
+        )
+        return left, right
+
+def parse_rules(lines: list[str]) -> Node:
+    raw_workflows = {
+        parts[0]: parts[1][:-1].split(",")
+        for line in lines
+        if (parts := line.split("{", 1))
+    }
+    
+    @ft.cache
+    def parse_node(workflow_name: str, index: int) -> Node:
+        if workflow_name == "A":
+            return AcceptedNode()
+        if workflow_name == "R":
+            return RejectedNode()
+        definition = raw_workflows[workflow_name][index]
+        if ":" not in definition:
+            return PassthroughNode(workflow_name, index, parse_node(definition, 0))
+        cond, target = definition.split(":", 1)
+        left = parse_node(target, 0)
+        right = parse_node(workflow_name, index+1)
         if "<" in cond:
-            name, val = cond.split("<", 1)
-            val = int(val)
-            new_ratings = ratings.copy()
-            new_ratings[name] = (new_ratings[name][0], val-1)
-            ans += simulate(new_ratings, workflows, nxt)
-            ratings[name] = (val, ratings[name][1])
+            name, x = cond.split("<")
+            return LessNode(workflow_name, index, name, int(x), left, right)
         else:
-            name, val = cond.split(">", 1)
-            val = int(val)
-            new_ratings = ratings.copy()
-            new_ratings[name] = (val+1, new_ratings[name][1])
-            ans += simulate(new_ratings, workflows, nxt)
-            ratings[name] = (ratings[name][0], val)
-    return ans
-        
+            name, x = cond.split(">")
+            return GreaterNode(workflow_name, index, name, int(x), left, right)
 
-
-
+    return parse_node("in", 0)
 
 with PuzzleContext(year=2023, day=19) as ctx:
-    ans1, ans2 = None, None
+    root_rule = parse_rules(ctx.groups[0].split("\n"))
+    part_ratings = list(map(PartRatings.parse, ctx.groups[1].split("\n")))
 
-    def parse_one(s: str) -> tuple[Optional[str], str]:
-        if ":" in s:
-            parts = s.split(":")
-            return parts[0], parts[1]
-        return (None, s)
+    ans1 = sum(pr.total for pr in part_ratings if root_rule.evaluate(pr) > 0)
+    ctx.submit(1, str(ans1))
 
-    workflows_str, parts = ctx.groups
-    workflows = dict()
-    for l in workflows_str.split("\n"):
-        name, rest = l.split("{", 1)
-        rest = rest[:-1]
-        conds = [parse_one(s) for s in rest.split(",")]
-        workflows[name] = conds
-        
-    ans1 = 0
-    for l in parts.split('\n'):
-        l = l[1:-1]
-        ratings = dict()
-        for s in l.split(","):
-            parts = s.split("=")
-            ratings[parts[0]] = int(parts[1])
-        s = sum(list(ratings.values()))
-        if is_accepted(ratings, workflows):
-            ans1 += s
-    ctx.submit(1, str(ans1) if ans1 else None)
-
-    ratings = {"x": (1, 4000),"m": (1, 4000),"a": (1, 4000),"s": (1, 4000)}
-
-    ans2 = simulate(ratings, workflows, "in")
-    ctx.submit(2, str(ans2) if ans2 else None)
+    r = Range(1, 4000)
+    ans2 = root_rule.evaluate(PartRatings(r,r,r,r))
+    ctx.submit(2, str(ans2))
